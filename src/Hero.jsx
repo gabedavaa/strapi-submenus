5e8b3b39ff9cfaf3747c3da48df7e6de5e41e53a
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-05-31 06:22:05 +0400

    adding components

---
+++ src/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          manage any content <br /> anywhere
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          quibusdam ab labore tempora quidem! Similique perferendis officiis
+          velit consequuntur.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
