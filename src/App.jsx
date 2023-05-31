59c753302763bd8f97833ddf623e7e3fd06e823f
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-05-31 09:04:28 +0400

    adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useGlobalContext } from "./Context";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
-// import Sidebar from "./Sidebar";
-// import Submenu from "./Submenu";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
-      {/* <Sidebar />
-      <Submenu /> */}
+      <Sidebar />
+      <Submenu />
     </main>
   );
 };
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./Context";
+import NavLinks from "./NavLinks";
 
 const Navbar = () => {
-  const { openSidebar } = useGlobalContext();
+  const { openSidebar, setPageId } = useGlobalContext();
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains("nav-link")) {
+      setPageId(null);
+    }
+  };
+
   return (
-    <nav>
+    <nav onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <h3 className="logo">straip</h3>
         <button className="toggle-btn" onClick={openSidebar}>
           <FaBars />
         </button>
+        <NavLinks />
       </div>
     </nav>
   );
